92c3effac613b05a9652f8e0420f6a0b9431120b
javierbarrera574 <82546878+javierbarrera574@users.noreply.github.com>
2022-08-29 00:38:21 -0300

    Agregar archivos de proyecto.

---
+++ DespensaDelBarrio/Server/Program.cs
@@ -0,0 +1,53 @@
+//using DespensaBarrial.BD.Datos;
+
+using DespensaBarrial.BD.Datos;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+
+
+var DespensaDelBarrioConnection = builder.Configuration.
+    GetConnectionString("DefaultConnection");
+
+
+builder.Services.
+    AddDbContext<DespensaDelBarrioDbContext>(opciones => opciones.
+    UseSqlServer(DespensaDelBarrioConnection)
+    );
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+
+app.MapRazorPages();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();
